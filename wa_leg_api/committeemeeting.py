4fc69a241e7b62bb188fbc77f0af8a00498ef547
j-carson <44308120+j-carson@users.noreply.github.com>
2021-02-08 18:56:18 -0800

    tweaked make_stubs to have source better for sphinx docs

--- wa_leg_api/committeemeeting.py
+++ wa_leg_api/committeemeeting.py
@@ -1,21 +1,21 @@
 from typing import Dict
 from datetime import datetime  # noqa
-from . import waleg
+from wa_leg_api import waleg
 
 
 def get_committee_meetings(begin_date: datetime, end_date: datetime) -> Dict:
     """See: http://wslwebservices.leg.wa.gov/committeemeetingservice.asmx?op=GetCommitteeMeetings"""
     argdict = dict(beginDate=begin_date, endDate=end_date)
-    return waleg.call("CommitteeMeeting", "GetCommitteeMeetings", argdict=argdict)
+    return waleg.call("CommitteeMeeting", "GetCommitteeMeetings", argdict)
 
 
 def get_revised_committee_meetings(changed_since_date: datetime) -> Dict:
     """See: http://wslwebservices.leg.wa.gov/committeemeetingservice.asmx?op=GetRevisedCommitteeMeetings"""
     argdict = dict(changedSinceDate=changed_since_date)
-    return waleg.call("CommitteeMeeting", "GetRevisedCommitteeMeetings", argdict=argdict)
+    return waleg.call("CommitteeMeeting", "GetRevisedCommitteeMeetings", argdict)
 
 
 def get_committee_meeting_items(agenda_id: int) -> Dict:
     """See: http://wslwebservices.leg.wa.gov/committeemeetingservice.asmx?op=GetCommitteeMeetingItems"""
     argdict = dict(agendaId=agenda_id)
-    return waleg.call("CommitteeMeeting", "GetCommitteeMeetingItems", argdict=argdict)
+    return waleg.call("CommitteeMeeting", "GetCommitteeMeetingItems", argdict)
